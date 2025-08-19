fa461559c5cc3c5ba65bb581ed5aebc235c29d5a
it13manoj <it13manoj@gmail.com>
2025-08-19 14:01:34 +0530

    udpate

--- src/API.js
+++ src/API.js
@@ -1,2 +1,6 @@
+// export const API ="https://api.parakshtach.com/api/"
+// export const Images ="https://api.parakshtach.com/uploads"
+
+
 export const API ="https://api.parakshtach.com/api/"
 export const Images ="https://api.parakshtach.com/uploads"
